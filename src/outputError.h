40e3d4032e52999907213c0b0eac348fd0f68039
Kenji <devnull@localhost>
2013-05-14 16:38:04 -0400

    US: As a user I would like to have a console user interface to compress a list of files or directories.

---
+++ src/outputError.h
@@ -0,0 +1,18 @@
+
+#ifndef OUTPUTERROR_H
+#define	OUTPUTERROR_H
+
+#define OPTION_ERROR "Zipper:unknown option\n"
+
+#define ARGS_ERROR "Zipper:Input arguments error\n"
+
+#define FILE_NOT_FOUND_ERROR "Zipper:File not Found\n"
+
+#define COMPRESSION_NOT_SUPPORTED_ERROR "Zipper: Compression Method Not Supported\n"
+
+#define CORRUPT_FILE_ERROR "Zipper:Current File is Corrupted\n"
+
+#define OK_PROCESS "Zipper: Process Success\n"
+
+#endif	/* OUTPUTERROR_H */
+
