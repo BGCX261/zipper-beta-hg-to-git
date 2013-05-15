fe2054ff623154cb427eff57e2ed3c0219af4a55
luis <luis@laguirrem-nh01>
2013-05-15 18:38:52 -0400

    US: Us a developer I want to write the file header structure. Description: I fix the tests

--- test/compressor/fileheadertest.cpp
+++ test/compressor/fileheadertest.cpp
@@ -7,64 +7,73 @@
  */
 
 #include <string.h>
+#include <stdlib.h>
 #include "fileheadertest.h"
 #include "../../src/compressor/fileheader.h"
 
 
 CPPUNIT_TEST_SUITE_REGISTRATION(FileHeaderTest);
 
-FileHeader* fh;
+FileHeader* header;
 
-FileHeaderTest::FileHeaderTest() {
+FileHeaderTest::FileHeaderTest()
+{
 }
 
-FileHeaderTest::~FileHeaderTest() {
+FileHeaderTest::~FileHeaderTest()
+{
 }
 
-void FileHeaderTest::setUp() {
-    fh = new FileHeader();
+void FileHeaderTest::setUp()
+{
+    header = new FileHeader();
 }
 
-void FileHeaderTest::tearDown() {
-    delete fh;
+void FileHeaderTest::tearDown()
+{
+    delete header;
+    header = 0;
 }
 
-void FileHeaderTest::testStructure() {
-    
-    fh->fileName = "File1";
-    fh->versionToExtract = 10;
-    fh->compressionMethod = 0;
-    fh->flag = 10;
-    fh->crc = 123456;
-    fh->lastModificationTime = 34568;
-    fh->lastModificationDate = 24963;
-    fh->compressedSize = 12;
-    fh->unCompressedSize = 12;
-    
-    FileHeader fh2 = *fh;
-    
-    CPPUNIT_ASSERT(*fh == fh2);
-    CPPUNIT_ASSERT(fh->headerSignature == fh2.headerSignature);
-    CPPUNIT_ASSERT(fh->versionToExtract == fh2.versionToExtract);
-    CPPUNIT_ASSERT(fh->compressionMethod == fh2.compressionMethod);
-    CPPUNIT_ASSERT(fh->flag == fh2.flag);
-    CPPUNIT_ASSERT(fh->crc == fh2.crc);
-    CPPUNIT_ASSERT(fh->lastModificationTime == fh2.lastModificationTime);
-    CPPUNIT_ASSERT(fh->lastModificationDate == fh2.lastModificationDate);
-    CPPUNIT_ASSERT(fh->compressedSize == fh2.compressedSize);
-    CPPUNIT_ASSERT(fh->unCompressedSize == fh2.unCompressedSize);
+void FileHeaderTest::testStructure()
+{
+
+    header->fileName = "File1";
+    header->versionToExtract = 10;
+    header->compressionMethod = 0;
+    header->flag = 10;
+    header->crc = 123456;
+    header->lastModificationTime = 34568;
+    header->lastModificationDate = 24963;
+    header->compressedSize = 12;
+    header->unCompressedSize = 12;
+
+    FileHeader fh2 = *header;
+
+    CPPUNIT_ASSERT(*header == fh2);
+    CPPUNIT_ASSERT(header->headerSignature == fh2.headerSignature);
+    CPPUNIT_ASSERT(header->versionToExtract == fh2.versionToExtract);
+    CPPUNIT_ASSERT(header->compressionMethod == fh2.compressionMethod);
+    CPPUNIT_ASSERT(header->flag == fh2.flag);
+    CPPUNIT_ASSERT(header->crc == fh2.crc);
+    CPPUNIT_ASSERT(header->lastModificationTime == fh2.lastModificationTime);
+    CPPUNIT_ASSERT(header->lastModificationDate == fh2.lastModificationDate);
+    CPPUNIT_ASSERT(header->compressedSize == fh2.compressedSize);
+    CPPUNIT_ASSERT(header->unCompressedSize == fh2.unCompressedSize);
 }
 
 void FileHeaderTest::testSetData()
 {
     const char* data = "sample data";
-    fh->setData(data, 11);
-    CPPUNIT_ASSERT(strcmp(data, fh->data) == 0);
+    const int dataLength = strlen(data);
+    header->setData(data, dataLength);
+    CPPUNIT_ASSERT(memcmp(data, header->data, dataLength) == 0);
 }
 
 void FileHeaderTest::testSetExtraField()
 {
-    const char* extraField = "Field to compress";
-    fh->setExtraField(extraField, 17);
-    CPPUNIT_ASSERT(strcmp(extraField, fh->extraField) == 0);
+    const char* field = "Field to compress";
+    const int extraFieldLength = strlen(field);
+    header->setExtraField(field, extraFieldLength);
+    CPPUNIT_ASSERT(memcmp(field, header->extraField, extraFieldLength) == 0);
 }
