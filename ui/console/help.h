2f39f22492e2596134746ba3a203d592a29ed74a
Kenji <devnull@localhost>
2013-05-16 16:22:03 +0200

    US:As a user I would like to have a console user interface to compress a list of files or directories. Description: Added the help for compression option with an example

--- ui/console/help.h
+++ ui/console/help.h
@@ -2,10 +2,27 @@
 #ifndef HELP_H
 #define	HELP_H
 
-const char* HELP_TEXT = "Zipper Help\n\
-	Options :\n \
-	-c [Source Paths...] [Destiny Path] [Compression Method] Compress a File(s) or(and) Directory\n\
-        -h                    Help\n";
+#define APLICATION_DESCRIPTION "Zipper Help:\n"
+
+#define COMPRESSION_DESCRIPTION "Compress(-c): \n\
+                          \tThat option compress a list of file and directories\n\
+                          \tand has the following format.\n\n\
+                          \t-c [Source Paths...] [Destiny Path] [Compression Method]\n\
+                          \tWhere:\n\
+                          \t[Source Paths...] The Files and Directories to will be compressed\n\
+                          \t[Destiny Path] The location where the zip file will be stored\n"
+#define COMPRESSION_EXAMPLE "\t\t\t\tExample:\n\
+                             \tIn order to use the compression(-c) option, \n\
+                             \tyou can look the following example:\n\
+                             \tIf i want to compress the following files:\n\
+                             \tusr/docs/myFile.txt\n\
+                             \tusr/docs/myFolder\n\
+                             \tIn this location: usr/compress\n\
+                             \tI should do the following:\n\
+                             \tzipper -c usr/docs/myFile.txt usr/docs/myFolder usr/compress\n"
+
+#define HELP_DESCRIPTION "Help(-h):\n\
+                          \tDisplay the help text of all options\n"
 
 #endif	/* HELP_H */
 
