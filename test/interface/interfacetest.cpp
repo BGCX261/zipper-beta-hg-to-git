03a2b7a7058876a317f17e902cab61c024b0fff5
kenji <kenji@GLOZANO-NH01>
2013-06-05 16:48:32 -0400

    Fixed tests

--- test/interface/interfacetest.cpp
+++ test/interface/interfacetest.cpp
@@ -28,6 +28,8 @@ void InterfaceTest::tearDown()
 
 void InterfaceTest::testCompressionTask()
 {
-    ErrorCode code = compress(NULL, NULL, 0, 0);   
+    char** input=new char*[1];
+    input[0]=(char*)"nonExistantFile";
+    ErrorCode code = compress((char*)"nonExistantDir",input, 1, 0);   
     CPPUNIT_ASSERT_EQUAL(FILE_NOT_FOUND, code);
 }
